038e25a4d97b7ec5a08ebd5afe4c9f2e56ff6b88
Szilágyi György <hello@0xc0defeed.com>
2013-11-01 23:44:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Képernyőkép
+----------
+
+![img](screenshots/default.png)
+
+![img](screenshots/media.png)
+
+További információért látogass el a [0xC0DEFEED](http://0xc0defeed.com/awesome-wm-dark-theme) blogra. 
---
+++ themes/dark/theme.lua
@@ -0,0 +1,12 @@
+theme = {}
+
+theme.font          = "Monaco 10"
+
+theme.fg_normal     = "#aaaaaa"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+
+theme.tasklist_disable_icon = true
+
+return theme
